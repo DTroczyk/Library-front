984e134456a0629ac84a803fea12a60e10c35e75
DTroczyk <DTroczyk@gmail.com>
2022-01-03 19:42:53 +0100

    add item preview

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import './Item.css'
+
+const BoardGame = {
+  author: '',
+  authorId: 0,
+  id: 0,
+  title: '',
+  description: '',
+  photo: '',
+  language: '',
+  yearOfRelease: 2010,
+  publishingHouse: '',
+  type: 'BoardGame',
+  owner: '',
+  ownerId: 0,
+  shelfId: '',
+  isPrivate: false,
+  
+  minPlayers: 1,
+  maxPlayers: 5,
+  length: '2h',
+  minAge: 16,
+  
+  isBorrowed: false,
+  isToLet: true,
+  borrower: '',
+  borrowerId: 0,
+}
+
+const Item = () => {
+
+  const itemType = () => {
+    switch (BoardGame.type) {
+      case 'BoardGame':
+        return 'Gra Planszowa'
+      case 'Book':
+        return 'Książka'
+      default:
+        return 'Nieznany typ'
+    }
+  }
+
+  return (
+    <div className='item-preview' title={itemType()}>
+      <div className='item-image'><img alt='Okładka'/></div>
+      <div className='item-name'>Terraformacja Marsa</div>
+    </div>
+  )
+}
+
+export default Item;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 
+import Item from '../Item/Item';
+
+import './Main.css'
+
 const Main = () => {
   return (
     <div>
-      <h2>Strona Główna</h2>
+      <h2>Książki i gry planszowe do wypożyczenia:</h2>
+      <div className='items-container'>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+        <Item/>
+      </div>
     </div>
   )
 }
